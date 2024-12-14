1fb2614e28935e6d83d8633b305c4d21519c0d3f
Chaewon Jeong <fromnowwon@gmail.com>
2024-12-14 20:09:07 +0900

    feat: fade in 애니메이션 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    keyframes: {
+      fadeIn: {
+        "0%": { opacity: 0 },
+        "100%": { opacity: 1 },
+      },
+    },
+    animation: { "fade-in": "fadeIn 3s ease-in-out" },
   },
   plugins: [],
 };
