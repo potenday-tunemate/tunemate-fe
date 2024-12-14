648d86580e19c34c1746d7c688979a5f2711f78b
Chaewon Jeong <fromnowwon@gmail.com>
2024-12-14 22:28:01 +0900

    chore: Pretendard font 추가 및 config 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,32 @@ module.exports = {
   darkMode: ["class"],
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        base: ["Pretendard-Regular", "sans-serif"],
+        bold: ["Pretendard-Bold", "sans-serif"],
+        semibold: ["Pretendard-SemiBold", "sans-serif"],
+        extrabold: ["Pretendard-Extrabold", "sans-serif"],
+      },
+      colors: {
+        tunemate: {
+          DEFAULT: "var(--tunemate, #563AE2)",
+          dark: "#323246",
+        },
+        black: "#0E0F13",
+        red: "#FF3D46",
+        customGray: {
+          900: "#0E0F13",
+          800: "#262629",
+          700: "#353538",
+          650: "#5D5D5E",
+          600: "#5E5E60",
+          500: "#A9A9A9",
+          400: "#D9D9DA",
+          300: "#E4E4E4",
+        },
+      },
+    },
     keyframes: {
       fadeIn: {
         "0%": { opacity: 0 },
