cf520ea7ad104cf37b6e0d16f56627d66bc5b9de
Chaewon Jeong <fromnowwon@gmail.com>
2024-12-14 20:10:08 +0900

    feat: 페이지 정의 및 스플래시 로고 애니메이션 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SplashPage from "./pages/Splash";
+import OnboardingPage from "./pages/Onboard";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="max-w-[600px] mx-auto min-h-[100vh] bg-black">
+      <Router>
+        <Routes>
+          <Route path="/" element={<SplashPage />} />
+          <Route path="/onboarding" element={<OnboardingPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
