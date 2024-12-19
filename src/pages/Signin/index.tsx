860e20ccfe67d0ae97a45ab07bfb4adb4008ca65
Chaewon Jeong <fromnowwon@gmail.com>
2024-12-19 10:54:35 +0900

    feat: 회원가입, 로그인 페이지 레이아웃 수정

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,10 +1,10 @@
 import { ASSET_PATHS } from "entities/const/path";
 import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { postSignin } from "shared/apis/signin/index.api";
 import FormInput from "shared/components/forms/FormInput";
 import Button from "shared/components/UI/Buttons/Button";
-import Header from "shared/components/UI/Header";
+import Header from "shared/components/Layout/Header";
 import { PostSigninRequestBody } from "shared/types/signin/index.type";
 
 export default function SigninPage() {
@@ -38,7 +38,7 @@ export default function SigninPage() {
   };
 
   return (
-    <>
+    <div className="flex flex-col h-full">
       <Header>
         <button
           className="flex w-[32px] h-[46px] p-[10px]"
@@ -51,63 +51,72 @@ export default function SigninPage() {
           />
         </button>
       </Header>
-      <div className="h-full pb-[50px] px-6">
-        <div className="relative h-full">
-          <div className="mt-[30px]">
-            <h2 className="text-[22px] font-bold">회원가입</h2>
 
-            <div className="mt-[34px]">
-              <div className="h-full">
-                <FormProvider {...methods}>
-                  <div className="flex flex-col space-y-8">
-                    <div>
-                      <FormInput
-                        label="아이디"
-                        name="email"
-                        register={register}
-                        errors={errors.email}
-                        placeholder="가입 시 등록한 이메일 주소를 입력해 주세요."
-                        required
-                        validationOptions={{
-                          required: "이메일 주소를 입력해 주세요.",
-                          pattern: {
-                            value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                            message: "올바른 이메일 형식을 입력해 주세요.",
-                          },
-                        }}
-                      />
-                    </div>
+      <div className="h-full pb-[80px] px-6">
+        <div className="relative h-full mt-[30px]">
+          <h2 className="text-[22px] font-bold">로그인</h2>
 
-                    <div>
-                      <FormInput
-                        label="비밀번호"
-                        type="password"
-                        name="password"
-                        register={register}
-                        errors={errors.password}
-                        placeholder="비밀번호"
-                        required
-                        validationOptions={{
-                          required: "비밀번호를 입력해 주세요.",
-                        }}
-                      />
-                    </div>
-                  </div>
-                  <div className="absolute bottom-0 left-0 w-full">
-                    <Button
-                      type="submit"
-                      onClick={handleSubmit(onSubmit)}
-                      disabled={isSubmitting || Object.keys(errors).length > 0}
-                    >
-                      로그인
-                    </Button>
-                  </div>
-                </FormProvider>
+          <div className="mt-[34px]">
+            <FormProvider {...methods}>
+              <div className="flex flex-col space-y-8">
+                <div>
+                  <FormInput
+                    label="아이디"
+                    name="email"
+                    register={register}
+                    errors={errors.email}
+                    placeholder="가입 시 등록한 이메일 주소를 입력해 주세요."
+                    required
+                    validationOptions={{
+                      required: "이메일 주소를 입력해 주세요.",
+                      pattern: {
+                        value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                        message: "올바른 이메일 형식을 입력해 주세요.",
+                      },
+                    }}
+                  />
+                </div>
+
+                <div>
+                  <FormInput
+                    label="비밀번호"
+                    type="password"
+                    name="password"
+                    register={register}
+                    errors={errors.password}
+                    placeholder="비밀번호"
+                    required
+                    validationOptions={{
+                      required: "비밀번호를 입력해 주세요.",
+                    }}
+                  />
+                </div>
               </div>
-            </div>
+
+              <div className="absolute bottom-0 left-0 w-full">
+                <div className="flex justify-center">
+                  <Link
+                    to="#"
+                    className="text-white text-[10px] hover:underline"
+                  >
+                    아이디/비밀번호를 잊으셨나요?
+                  </Link>
+                </div>
+
+                <div className="mt-[15px]">
+                  <Button
+                    type="submit"
+                    onClick={handleSubmit(onSubmit)}
+                    disabled={isSubmitting || Object.keys(errors).length > 0}
+                  >
+                    로그인
+                  </Button>
+                </div>
+              </div>
+            </FormProvider>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
