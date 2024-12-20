664daeea3c1d89c4df770d5bc537be3895f52ade
Chaewon Jeong <fromnowwon@gmail.com>
2024-12-20 19:24:16 +0900

    feat: 404 페이지 추가

---
+++ src/pages/NotFound/index.tsx.tsx
@@ -0,0 +1,10 @@
+function NotFoundPage() {
+  return (
+    <div className="flex flex-col justify-center items-center py-40">
+      <h1>404 Not Found</h1>
+      <p>페이지를 찾을 수 없습니다.</p>
+    </div>
+  );
+}
+
+export default NotFoundPage;
