1e4f5fff0ffdd83fc7849f867f80b1fcf1eab705
Chaewon Jeong <fromnowwon@gmail.com>
2024-12-24 01:29:17 +0900

    chore: 레이아웃 수정

--- src/shared/components/Layout/Container.tsx
+++ src/shared/components/Layout/Container.tsx
@@ -3,5 +3,7 @@ interface ContainerProps {
 }
 
 export default function Container({ children }: ContainerProps) {
-  return <div className="relative w-full h-full px-24 pb-100">{children}</div>;
+  return (
+    <div className="relative w-full h-screen px-24 pb-100">{children}</div>
+  );
 }
--- src/widgets/main/ui/MainHeader/index.tsx
+++ src/widgets/main/ui/MainHeader/index.tsx
@@ -3,7 +3,7 @@ import { ASSET_PATHS } from "shared/constants/path";
 
 export default function MainHeader() {
   return (
-    <header className="fixed top-0 left-1/2 -translate-x-1/2 w-full max-w-600 mx-auto flex items-center h-58 px-24">
+    <header className="fixed top-0 left-1/2 -translate-x-1/2 w-full max-w-600 mx-auto flex items-center h-58 px-24 bg-black z-20">
       <Link to="/main">
         <img
           src={`${ASSET_PATHS.ICONS}/logo/tunemate_logo_symbol.svg`}
