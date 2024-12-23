8d8c0084abc155fe8e2e7382ea9a0b1dc5690172
Chaewon Jeong <fromnowwon@gmail.com>
2024-12-24 02:42:40 +0900

    feat: 가입 완료 페이지 추가

--- src/pages/auth/sign-up/welcome/index.tsx
+++ src/pages/auth/sign-up/welcome/index.tsx
@@ -1,27 +1,23 @@
-import { Link } from "react-router-dom";
 import Container from "shared/components/layout/Container";
-import Header from "shared/components/layout/Header";
-import Button from "shared/components/ui/buttons/Button";
 import LinkButton from "shared/components/ui/buttons/LinkButton";
 
 export default function WelcomePage() {
   return (
-    <div className="h-full">
-      <Header />
-      <div className="mt-74">
-        <Container>
-          <div className="relative flex flex-col items-center h-full">
-            <p>가입 완료</p>
-            <p>
-              <span className="text-tunemate">아름다운 멜로디</span>님,
-              튠메이트에 오신걸 환영해요
-            </p>
+    <div className="flex flex-col h-screen px-24">
+      <div className="pt-74">
+        <p className="font-bold text-22 text-center">가입 완료</p>
+      </div>
 
-            <div className="absolute bottom-0">
-              <LinkButton path="/main">로그인 화면으로 이동</LinkButton>
-            </div>
-          </div>
-        </Container>
+      <div className="flex items-center justify-center flex-grow mt-[-140px]">
+        <p className="font-bold text-22 text-center">
+          <span className="text-tunemate">아름다운 멜로디</span>님,
+          <br />
+          튠메이트에 오신걸 환영해요
+        </p>
+      </div>
+
+      <div className="sticky bottom-80 p-4">
+        <LinkButton path="/auth/sign-in">로그인 화면으로 이동</LinkButton>
       </div>
     </div>
   );
