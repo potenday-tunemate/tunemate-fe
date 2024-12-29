e5fe083a122443cab2b5ae432f3e7d103877b0fe
Sungjin Lee <sjlee990129@gmail.com>
2024-12-29 17:49:14 +0900

    Update index.tsx

--- src/pages/recent-reviews/index.tsx
+++ src/pages/recent-reviews/index.tsx
@@ -1,6 +1,6 @@
-import Container from "shared/components/layout/Container";
-import Header from "shared/components/layout/Header";
-import PageLayout from "shared/components/layout/PageLayout";
+import Container from "shared/components/Layout/Container";
+import Header from "shared/components/Layout/Header";
+import PageLayout from "shared/components/Layout/PageLayout";
 import { RECENT_REVIEWS_DATA } from "widgets/main/data";
 
 export default function RecentReviewsPage() {
