35bf58d6a2f1f4f6be074ad5fdc6e1bd6ead2a52
Sungjin Lee <sjlee990129@gmail.com>
2024-12-29 17:57:05 +0900

    Update index.tsx

--- src/pages/auth/sign-up/index.tsx
+++ src/pages/auth/sign-up/index.tsx
@@ -1,6 +1,6 @@
-import Container from "shared/components/layout/Container";
-import Header from "shared/components/layout/Header";
-import PageLayout from "shared/components/layout/PageLayout";
+import Container from "shared/components/Layout/Container";
+import Header from "shared/components/Layout/Header";
+import PageLayout from "shared/components/Layout/PageLayout";
 import SignUpForm from "widgets/auth/sign-up/ui/SignUpForm";
 
 export default function SignUpPage() {
